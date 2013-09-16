c7d5f9a138ae891bfcc5bba100386ab4bf126761
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-09-16 10:23:29 +0000

    Update copyright text

--- modules/core/src/com/haulmont/fts/core/sys/EntityDescr.java
+++ modules/core/src/com/haulmont/fts/core/sys/EntityDescr.java
@@ -1,12 +1,6 @@
 /*
- * Copyright (c) 2010 Haulmont Technology Ltd. All Rights Reserved.
- * Haulmont Technology proprietary and confidential.
- * Use is subject to license terms.
-
- * Author: Konstantin Krivopustov
- * Created: 20.07.2010 16:26:08
- *
- * $Id$
+ * Copyright (c) 2008-2013 Haulmont. All rights reserved.
+ * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
  */
 package com.haulmont.fts.core.sys;
 
